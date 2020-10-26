af35753ff6f0f6126eb06b937819009765bdda2b
Divyansh Jain <wintendoin@gmail.com>
2020-10-26 14:46:29 +0530

    Add files via upload

---
+++ message_room.js
@@ -0,0 +1,54 @@
+//ADD YOUR FIREBASE LINKS
+var firebaseConfig = {
+  apiKey: "AIzaSyB9G5mJNgmkwpLF-8ar8u4QQ0EVC41FIlc",
+  authDomain: "talk-and-chat-186ab.firebaseapp.com",
+  databaseURL: "https://talk-and-chat-186ab.firebaseio.com",
+  projectId: "talk-and-chat-186ab",
+  storageBucket: "talk-and-chat-186ab.appspot.com",
+  messagingSenderId: "307954488325",
+  appId: "1:307954488325:web:be504923a9cded757a386c",
+  measurementId: "G-WT1PCYRG55"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "message_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "message_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
